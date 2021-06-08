54658a827a9950931c05c21ab6638d0cc8d62bfc
Lucas <lucas.p.oliveira@opah.com.br>
2021-06-08 19:55:37 -0300

    Data annotations

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,6 +10,8 @@ using System.Threading.Tasks;
 
 namespace App.Controllers
 {
+    [Route("")]
+    [Route("gestao")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,16 +21,24 @@ namespace App.Controllers
             _logger = logger;
         }
 
+        [Route("")]
+        [Route("inicio/{id:int?}")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("termo")]
         public IActionResult Privacy()
         {
-            return View();
+            //return Json(new { Name = "Lucas", Status = true});
+            var fileBytes = System.IO.File.ReadAllBytes(@"C:/db_app/Cliente_db.csv");
+            var name = "text.csv";
+
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, name);
         }
 
+        [Route("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ App/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title cannot be null")]
+        public string Title { get; set; }
+        [DataType(DataType.DateTime, ErrorMessage = "Your data format is wrong, please, use yyyy/mm/dd")]
+        [Required(ErrorMessage = "Realese date cannot be null")]
+        public DateTime ReleasedAt { get; set; }
+        [StringLength(maximumLength:30,MinimumLength = 3, ErrorMessage = "Please, the title must have Maximum Length: 30, Minimum Length: 3")]
+        [RegularExpression(@"^[A-Z]+[a-z-A-Z\u00c0-\u00FF""'\w-*$", ErrorMessage = "Gender in incorrect format")]
+        public string Gender { get; set; }
+        [Range(minimum:1, maximum: 30000, ErrorMessage = "The value must be between 1 and 30000")]
+        [Required(ErrorMessage = "Price cannot be null")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[1-1000]*$",ErrorMessage ="The rate must be a number")]
+        [Range(minimum:1, maximum:5, ErrorMessage = "The rate must be between 1 and 5")]
+        [Required(ErrorMessage = "Rate cannot be null")]
+        public int Rate { get; set; }
+        [StringLength(maximumLength: 70, MinimumLength = 10,ErrorMessage = "Please, the title must have Maximum Length: 70, Minimum Length: 10")]
+        [Required(ErrorMessage ="Description cannot be null")]
+        public string Description { get; set; }
+    }
+}
