5e273a71f08851b73225088a4a645c6e7156554f
Lucas <lucas.p.oliveira@opah.com.br>
2021-06-08 23:00:41 -0300

    Criando view de formulário

---
+++ App/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Controllers
+{
+    [Route("Movies")]
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+    }
+}
