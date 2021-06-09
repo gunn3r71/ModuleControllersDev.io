0ec7096b2421d1fa78ef43238e6a5b6a61f79e06
Lucas <lucas.p.oliveira@opah.com.br>
2021-06-08 21:17:16 -0300

    Trabalhando com view components

---
+++ App/ViewComponents/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.ViewComponents
+{
+    [ViewComponent(Name = "ShoppingCart")]
+    public class ShoppingCart : ViewComponent
+    {
+        public int ProductsAmount { get; set; }
+
+        public ShoppingCart()
+        {
+            ProductsAmount = 10;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ProductsAmount);
+        }
+    }
+}
